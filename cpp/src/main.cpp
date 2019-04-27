d567dfbef6cf956a507c6f5c2f3ea300a4b53f7e
TopQuark12 <tat1212wong@gmail.com>
2019-04-28 01:44:08 +0800

    init cpp commit, working makefile build system

---
+++ cpp/src/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file        main.c
+ * @author      Wong Tat Hang (Alex)    thwongaz@connect.ust.hk
+ * @brief       main entry for mesh processing and generation system built for 
+ *              ISDN2400, ISDN1003 and ISDN1005 integrative project
+ * @date        28/4/2019
+ * 
+ * @copyright   Copyright (c) 2019
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world";
+    return 0;
+}
