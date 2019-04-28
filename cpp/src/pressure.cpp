c27dd133eb8d8c0df6ac48261a07770594d2c8b3
TopQuark12 <tat1212wong@gmail.com>
2019-04-28 19:23:22 +0800

    protect against corrupt data

--- cpp/src/pressure.cpp
+++ cpp/src/pressure.cpp
@@ -16,6 +16,7 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
@@ -23,31 +24,46 @@ int pressureGen() {
 
     ifstream presMap;
     presMap.open("input/pressureMap.txt", ios::in);
-    #ifdef DEBUG_BUILD
     if(presMap.is_open()) {
         cout << "Pressure map found and opened \n";
     } else {
         cout << "Pressure map not found! \n";
         return 1;
     }
-    #endif
     vector<vector<int>> map;
+    vector<int> lineLength;
     string line;
+    int numLine = 0;
     while (getline(presMap, line)) {
+        numLine++;
         map.push_back({});
         istringstream iss(line);
         int num;
         while(iss >> num){
             map.back().push_back(num);
         }
+        lineLength.push_back(map.back().size());
+    }
+    if (numLine == 0) {
+        cout << "Pressure map empty!!\n";
+        return 2;
+    }
+    if (distance(lineLength.begin(), unique(lineLength.begin(), lineLength.end())) != 1)
+    {
+        cout << "Pressure map row length not consistent!!\n";
+        return 3;
     }
     #ifdef DEBUG_BUILD
+    cout << "Rows: " << numLine << '\n';
+    cout << "Cols: " << lineLength[0] << '\n';
+    cout << "Pressure map data: \n";
     for (auto i = map.begin(); i != map.end(); i++) {
         for (auto j = i->begin(); j != i->end(); j++) {
             cout << *j << '\t';
         }
         cout << '\n';
     }
+    cout << '\n';
     #endif
     presMap.close();
     return 0;
