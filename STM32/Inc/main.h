975bed563b23f24d37212ae1abcbd492a637e5df
TopQuark12 <tat1212wong@gmail.com>
2019-05-12 20:18:27 +0800

    init stm32 gen

---
+++ STM32/Inc/main.h
@@ -0,0 +1,221 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  ** This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * COPYRIGHT(c) 2019 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define USER_Btn_Pin GPIO_PIN_13
+#define USER_Btn_GPIO_Port GPIOC
+#define MCO_Pin GPIO_PIN_0
+#define MCO_GPIO_Port GPIOH
+#define RMII_MDC_Pin GPIO_PIN_1
+#define RMII_MDC_GPIO_Port GPIOC
+#define RMII_REF_CLK_Pin GPIO_PIN_1
+#define RMII_REF_CLK_GPIO_Port GPIOA
+#define RMII_MDIO_Pin GPIO_PIN_2
+#define RMII_MDIO_GPIO_Port GPIOA
+#define RMII_CRS_DV_Pin GPIO_PIN_7
+#define RMII_CRS_DV_GPIO_Port GPIOA
+#define RMII_RXD0_Pin GPIO_PIN_4
+#define RMII_RXD0_GPIO_Port GPIOC
+#define RMII_RXD1_Pin GPIO_PIN_5
+#define RMII_RXD1_GPIO_Port GPIOC
+#define R0_Pin GPIO_PIN_2
+#define R0_GPIO_Port GPIOB
+#define R1_Pin GPIO_PIN_11
+#define R1_GPIO_Port GPIOF
+#define R2_Pin GPIO_PIN_12
+#define R2_GPIO_Port GPIOF
+#define R3_Pin GPIO_PIN_13
+#define R3_GPIO_Port GPIOF
+#define R4_Pin GPIO_PIN_14
+#define R4_GPIO_Port GPIOF
+#define R5_Pin GPIO_PIN_15
+#define R5_GPIO_Port GPIOF
+#define R6_Pin GPIO_PIN_0
+#define R6_GPIO_Port GPIOG
+#define R7_Pin GPIO_PIN_1
+#define R7_GPIO_Port GPIOG
+#define R8_Pin GPIO_PIN_7
+#define R8_GPIO_Port GPIOE
+#define R9_Pin GPIO_PIN_8
+#define R9_GPIO_Port GPIOE
+#define R10_Pin GPIO_PIN_9
+#define R10_GPIO_Port GPIOE
+#define R11_Pin GPIO_PIN_10
+#define R11_GPIO_Port GPIOE
+#define R12_Pin GPIO_PIN_11
+#define R12_GPIO_Port GPIOE
+#define R13_Pin GPIO_PIN_12
+#define R13_GPIO_Port GPIOE
+#define R14_Pin GPIO_PIN_13
+#define R14_GPIO_Port GPIOE
+#define R15_Pin GPIO_PIN_14
+#define R15_GPIO_Port GPIOE
+#define R16_Pin GPIO_PIN_15
+#define R16_GPIO_Port GPIOE
+#define R17_Pin GPIO_PIN_10
+#define R17_GPIO_Port GPIOB
+#define R18_Pin GPIO_PIN_11
+#define R18_GPIO_Port GPIOB
+#define R19_Pin GPIO_PIN_12
+#define R19_GPIO_Port GPIOB
+#define RMII_TXD1_Pin GPIO_PIN_13
+#define RMII_TXD1_GPIO_Port GPIOB
+#define LD3_Pin GPIO_PIN_14
+#define LD3_GPIO_Port GPIOB
+#define C0_Pin GPIO_PIN_15
+#define C0_GPIO_Port GPIOB
+#define STLK_RX_Pin GPIO_PIN_8
+#define STLK_RX_GPIO_Port GPIOD
+#define STLK_TX_Pin GPIO_PIN_9
+#define STLK_TX_GPIO_Port GPIOD
+#define C1_Pin GPIO_PIN_10
+#define C1_GPIO_Port GPIOD
+#define C2_Pin GPIO_PIN_11
+#define C2_GPIO_Port GPIOD
+#define C3_Pin GPIO_PIN_12
+#define C3_GPIO_Port GPIOD
+#define C4_Pin GPIO_PIN_13
+#define C4_GPIO_Port GPIOD
+#define C5_Pin GPIO_PIN_14
+#define C5_GPIO_Port GPIOD
+#define C6_Pin GPIO_PIN_15
+#define C6_GPIO_Port GPIOD
+#define C7_Pin GPIO_PIN_2
+#define C7_GPIO_Port GPIOG
+#define C8_Pin GPIO_PIN_3
+#define C8_GPIO_Port GPIOG
+#define C9_Pin GPIO_PIN_4
+#define C9_GPIO_Port GPIOG
+#define C10_Pin GPIO_PIN_5
+#define C10_GPIO_Port GPIOG
+#define USB_PowerSwitchOn_Pin GPIO_PIN_6
+#define USB_PowerSwitchOn_GPIO_Port GPIOG
+#define USB_OverCurrent_Pin GPIO_PIN_7
+#define USB_OverCurrent_GPIO_Port GPIOG
+#define C11_Pin GPIO_PIN_8
+#define C11_GPIO_Port GPIOG
+#define C12_Pin GPIO_PIN_6
+#define C12_GPIO_Port GPIOC
+#define C13_Pin GPIO_PIN_7
+#define C13_GPIO_Port GPIOC
+#define C14_Pin GPIO_PIN_8
+#define C14_GPIO_Port GPIOC
+#define C15_Pin GPIO_PIN_9
+#define C15_GPIO_Port GPIOC
+#define USB_SOF_Pin GPIO_PIN_8
+#define USB_SOF_GPIO_Port GPIOA
+#define USB_VBUS_Pin GPIO_PIN_9
+#define USB_VBUS_GPIO_Port GPIOA
+#define USB_ID_Pin GPIO_PIN_10
+#define USB_ID_GPIO_Port GPIOA
+#define USB_DM_Pin GPIO_PIN_11
+#define USB_DM_GPIO_Port GPIOA
+#define USB_DP_Pin GPIO_PIN_12
+#define USB_DP_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define C16_Pin GPIO_PIN_15
+#define C16_GPIO_Port GPIOA
+#define C17_Pin GPIO_PIN_10
+#define C17_GPIO_Port GPIOC
+#define C18_Pin GPIO_PIN_11
+#define C18_GPIO_Port GPIOC
+#define C19_Pin GPIO_PIN_12
+#define C19_GPIO_Port GPIOC
+#define RMII_TX_EN_Pin GPIO_PIN_11
+#define RMII_TX_EN_GPIO_Port GPIOG
+#define RMII_TXD0_Pin GPIO_PIN_13
+#define RMII_TXD0_GPIO_Port GPIOG
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define LD2_Pin GPIO_PIN_7
+#define LD2_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
